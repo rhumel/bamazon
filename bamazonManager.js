7f7d487cb3881f42e7fa6ffeb4157c0f04f60a5d
rhumel <rhumel@hotmail.com>
2018-08-23 00:02:18 -0400

    add more code to switch statement to add inventory

--- bamazonManager.js
+++ bamazonManager.js
@@ -41,14 +41,16 @@ function initialPromts(){
                 case "View Products for Sale":
                     console.log("display product function");
                     displayProducts();
+                    initialPromts();
+                    break;
             
                 case "View Low Inventory":
                     lowInventory();
                     break;
             
-                // case "Add to Inventory":
-                //     addInventory();
-                //     break;
+                case "Add to Inventory":
+                    addInventory();
+                    break;
             
                 // case "Add New Product":
                 //     addProduct();
@@ -88,7 +90,7 @@ function displayProducts() {
 
             })
             console.log(t.toString());
-            initialPromts();  
+            // initialPromts();  
 
         }
     // console.log(query.sql);    
@@ -96,3 +98,100 @@ function displayProducts() {
     )
 };
 
+function addInventory() {
+    // Call display so the Manager can see what is available
+        
+    //    displayProducts();
+
+    // get categories for post
+    inquirer
+    .prompt([
+        {
+            name: "itemNumber",
+            type: "input",
+            message: "What is the number of the item you wish to increase the inventory?"
+        },
+        {
+            name: "purchaseQuantity",
+            type: "input",
+            message: "How many units should be added to inventory?"
+        }
+
+    ])
+
+    .then(function (answer) {
+        //
+        // Create and run QUERY to find the Item # of the product
+        //
+        var itemNumber = answer.itemNumber;
+        var quantity = answer.purchaseQuantity;
+        console.log(itemNumber);
+        console.log(quantity);
+
+        // Select the item from the db and get the db quantity
+        connection.query(
+            "SELECT * from products WHERE ?",
+            {
+                id: itemNumber
+            },
+            function (err, res) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    // get the new quantity to update the DB
+                    console.log(res[0].quantity);
+                    var prodQuant = parseInt(quantity) + parseInt(res[0].quantity);
+                    var product = res[0].product_name;
+                    // use the quantity and run SQL statement
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                          {
+                            quantity: prodQuant
+                          },
+                      
+                          {
+                            id: itemNumber
+                          }
+                        ],
+                        function (err, res) {
+                            if (err) {
+                                console.log(err)
+                            } else {
+                                // Check the User qty against DB.  If insufficient, send user mesg
+                                console.log("The inventory for "+ product +" has been increased to "+ prodQuant+".");
+                                }
+            
+                            }
+                    )}
+                }
+        )
+      })   
+
+
+// function addInventory() {
+
+//     // get categories for post
+//     console.log("Selecting all Available Items to add ...\n");
+//     connection.query("SELECT * FROM products", function (err, res) {
+//         if (err) {
+//             console.log(err);
+//             throw err;
+//         } else {
+//             // used for the easy-table package
+//             var t = new Table;
+
+//             // // Log all results of the SELECT statement
+//             res.forEach((productList, i) => {
+//                 // t.cell is used for easy-table set-up
+//                 t.cell('Product Id', productList.id)
+//                 t.cell('Department Name',productList.department_name)
+//                 t.cell('Description', productList.product_name)
+//                 t.cell('Price',productList.customer_price,Table.number(2))
+//                 t.cell('Quantity', productList.quantity)
+//                 t.newRow()
+
+//             })
+//             console.log(t.toString());
+//             initialPromts();  
+    }
